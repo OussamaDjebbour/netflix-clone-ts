8b3625e688bf782e467302a54dda273c5753f398
OussamaDjebbour <oussama_djebbour@outlook.com>
2024-12-21 02:41:23 +0100

    refactor the code

--- src/constants.ts
+++ src/constants.ts
@@ -9,17 +9,17 @@ export const TMDBBASEURL = 'https://api.themoviedb.org/3';
 export const TMDBORIGINALIMAGEURL = 'https://image.tmdb.org/t/p/original';
 export const TMDBIMAGEURLSMALLSIZE = 'https://image.tmdb.org/t/p/w500';
 
-// Genre configuration
-export const GENRE_CONFIG = {
-  INCLUDE_GENRES: [10751], // Family
-  EXCLUDE_GENRES: [10749, 35], // Romance and Comedy
-};
-
 // Media types
 export const MEDIA_TYPES = {
   MOVIE: 'movie',
   TV: 'tv',
 } as const;
+
+// Genre configuration
+export const GENRE_CONFIG = {
+  INCLUDE_GENRES: [10751], // Family
+  EXCLUDE_GENRES: [10749, 35], // Romance and Comedy
+};
 
 export const socialMediaLinks = [
   { icon: faFacebook, label: 'Facebook' },
